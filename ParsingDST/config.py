bba72f576b3736f92336e59705ddf07d0f9da9e1
yuxiangW <76482447+ToLightUpTheSky@users.noreply.github.com>
2023-11-03 18:50:45 +0800

    Update config.py

--- ParsingDST/config.py
+++ ParsingDST/config.py
@@ -1,8 +1,7 @@
 CONFIG = {
-        "api_key": ['sk-xHCRoxWH3cR2uVwFhNxp2pmWWHjNUXVrj72JkDySjB0ruUtd','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'],
-        "api_base": "https://api.chatanywhere.cn/v1",
-        "api_organization": "org-GKQJV0sEpDLwR1xsdpCm0KkU",
-        # "api_organization": "org-GKQJV0sEpDLwR1xsdpCm0KkU",
+        "api_key": [''], # Your openAI api-key
+        "api_base": "", # Your proxy 
+        "api_organization": "", # Your api_organization
         "ontology_21": "./data/mwz2.1/ontology.json",
         "ontology_24": "./data/mwz2.4/ontology.json",
 }
